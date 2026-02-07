2c8f6a009bf7927f805936f5f1d2f4dbe1ce6a2c
Cesahz <espinolac999@gmail.com>
2026-02-07 15:10:03 -0300

    Implementacion de sistema de validacion de movimiento

--- minimax_lab.py
+++ minimax_lab.py
@@ -19,6 +19,10 @@ class Peon:
         self.x = x
         self.y = y
         self.simbolo = simbolo
+    def cambiar_posicion(self,nueva_x,nueva_y):
+        #actualizamos las coordenadas
+        self.x = nueva_x
+        self.y = nueva_y
 
 class Gato(Peon):
     #gato malo, tiene que atrapar al raton para poder ganar
@@ -52,7 +56,7 @@ class Juego:
     def colocar_muro(self,x,y):
         #coloca un obstaculo (muro) en una coord especifica
         if 0 <= x < self.ancho and 0 <= y < self.alto:
-            self.tablero[x][y] = SIMBOLO_MURO
+            self.tablero[y][x] = SIMBOLO_MURO
 
     def renderizar(self):
         #este dibuja el estado de la consola en el presente de su ejecucion
@@ -71,26 +75,70 @@ class Juego:
             
             #controlar que no este fuera del tablero
             if 0 <= px < self.ancho and 0 <= py < self.alto:
-                tablero_visual[px][py] = peon.simbolo
+                tablero_visual[py][px] = peon.simbolo
                 
         for fila in tablero_visual:
             texto_fila = ' '.join(fila)
             print(texto_fila)
+    
+    def es_movimiento_valido(self,x,y):
+        #no salir del tablero
+        if x < 0:
+            return False
+        if y < 0:
+            return False
+        if x >= self.ancho:
+            return False
+        if y >= self.alto:
+            return False
+        #no chocar con el muro
+        if self.tablero[y][x] == SIMBOLO_MURO:
+            return False
+        
+        #si pasa todas la pruebas me devuelve true
+        return True
+
+
+
+
+
 
 if __name__ == "__main__":
-    #crea un tablero de 10*10
-    mi_juego = Juego(10,10)
+    # Crear juego y muro
+    mi_juego = Juego(10, 10)
+    mi_juego.colocar_muro(1, 0) # Ponemos un muro a la derecha de Tom (en x=1, y=0)
     
-    #poner muros manuales
-    mi_juego.colocar_muro(5,5)
-    mi_juego.colocar_muro(5,6)
-    mi_juego.colocar_muro(5,7)
-    
-    #crear los peones 
-    tom = Gato(0,0)
-    jerry = Raton(9,9)
+    # Crear peones
+    tom = Gato(0, 0)
+    jerry = Raton(9, 9)
     
     mi_juego.agregar_peon(tom)
     mi_juego.agregar_peon(jerry)
     
+    print("--- INICIO ---")
+    mi_juego.renderizar()
+    
+    # --- PRUEBA MANUAL DE MOVIMIENTO ---
+    input("Presiona Enter para intentar mover al Gato...")
+
+    # Intento 1: Mover a la derecha (x=1, y=0) -> ¡HAY UN MURO!
+    siguiente_x = 1
+    siguiente_y = 0
+    
+    if mi_juego.es_movimiento_valido(siguiente_x, siguiente_y):
+        print("Moviendo gato a la derecha...")
+        tom.cambiar_posicion(siguiente_x, siguiente_y)
+    else:
+        print("¡GOLPE! El gato chocó con un muro en (1, 0)")
+
+    # Intento 2: Mover hacia abajo (x=0, y=1) -> ESTÁ LIBRE
+    siguiente_x = 0
+    siguiente_y = 1
+    
+    if mi_juego.es_movimiento_valido(siguiente_x, siguiente_y):
+        print("Moviendo gato hacia abajo...")
+        tom.cambiar_posicion(siguiente_x, siguiente_y)
+    else:
+        print("¡GOLPE! El gato no pudo bajar")
+        
     mi_juego.renderizar()
